90ebe67b18a6e4d9e2701b842549e4d701b6ee03
ciusji <bqjimaster@gmail.com>
2022-08-29 11:21:21 +0800

    add help doc link

--- client/trino-cli/src/main/java/io/trino/cli/Help.java
+++ client/trino-cli/src/main/java/io/trino/cli/Help.java
@@ -33,7 +33,8 @@ public final class Help
                 "SHOW CATALOGS [LIKE <pattern>]\n" +
                 "SHOW SCHEMAS [FROM <catalog>] [LIKE <pattern>]\n" +
                 "SHOW TABLES [FROM <schema>] [LIKE <pattern>]\n" +
-                "USE [<catalog>.]<schema>\n" +
+                "USE [<catalog>.]<schema>\n\n" +
+                "Documentation: https://ciusji.gitbook.io/guinsoolab/products/query-engine/witdb/overview\n" +
                 "";
     }
 }
